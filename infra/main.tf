89fe9821f2a171f162e61c315a6cc66ae2e08341
Andrew Mak <andrew.cm.sp@gmail.com>
2023-01-01 18:33:40 -0300

    adiciona website

--- infra/main.tf
+++ infra/main.tf
@@ -7,25 +7,3 @@ provider "aws" {
 }
 
 
-resource "aws_s3_bucket" "state_terraform_aws" {
-  bucket = "infrawebsiteandrewchan"
-  acl    = "private"
-  
-  versioning {
-    enabled = true
-  }
-
-   tags = {
-    Description = "bucket para guardar o tf state"
-    EmailOwner  = "andrew.cm.sp@gmail.com"
-    Owner  = "andrew"
-   } 
-
-}
-output "bucket_name" {
-    value = aws_s3_bucket.state_terraform_aws.bucket
-}
-
-output "bucket_ARN" {
-    value = aws_s3_bucket.state_terraform_aws.arn
-}
