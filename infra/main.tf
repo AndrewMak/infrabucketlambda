ff6d8a8cf11602624f8d4819757c9793ebcc13b2
Andrew Chan Mak <andrew.cm.sp@gmail.com>
2023-01-01 20:08:41 +0000

    adiciona state remoto

--- infra/main.tf
+++ infra/main.tf
@@ -1,8 +1,33 @@
+terraform {
+  backend "s3" {
+  bucket  = "andrew-state-terraform"
+  key     = "tfstate/terraform.tfstate"
+  region  = "us-east-1"
+}
+
+}
 provider "aws" {
     region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "b" {
-  bucket = "tcb-blog-s3"
+
+resource "aws_s3_bucket" "state_terraform_aws" {
+  bucket = "andrew-state-terraform"
   acl    = "private"
+  
+  versioning {
+    enabled = true
+  }
+
+   tags = {
+    Description = "bucket para guardar o tf state"
+   } 
+
+}
+output "bucket_name" {
+    value = aws_s3_bucket.state_terraform_aws.bucket
+}
+
+output "bucket_ARN" {
+    value = aws_s3_bucket.state_terraform_aws.arn
 }
